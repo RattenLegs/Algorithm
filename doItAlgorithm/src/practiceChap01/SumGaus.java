42c1c754da819f78a2f96c8b21e1ae23214a2419
Rattanlegs <Rattanlegs@gmail.com>
2024-01-11 23:32:09 +0900

    문제 하나 해결하고 잘 것

--- doItAlgorithm/src/practiceChap01/SumGaus.java
+++ doItAlgorithm/src/practiceChap01/SumGaus.java
@@ -1,10 +1,17 @@
 package practiceChap01;
 
+import java.util.Scanner;
+
 public class SumGaus {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner stdIn = new Scanner(System.in);
+		
+		System.out.println("가우스 쓸거임. n 입력 ㄱ");
+		int n = stdIn.nextInt();
+		
+		int sum = (1+n)*n/2;
+		System.out.println(sum);
 	}
 
 }
--- doItAlgorithm/src/practiceChap01/SumOf.java
+++ doItAlgorithm/src/practiceChap01/SumOf.java
@@ -1,10 +1,41 @@
 package practiceChap01;
 
+import java.util.Scanner;
+
 public class SumOf {
 
+	static int sumof(int a, int b) {
+		int sum = 0; 
+		if(a>b) {
+			//for
+			for(int i=b; i<=a; i++) 
+				sum += i;
+		} else if(b>a) { 
+			//while
+			while(a<=b) {
+				sum += a;
+				a++;
+			}
+		} else {
+			sum = a+b;
+		}
+		return sum;
+	}
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner stdIn = new Scanner(System.in);
+		
+		//정수 입력
+		System.out.println("a 입력");
+		int a = stdIn.nextInt();
+		System.out.println("b 입력");
+		int b = stdIn.nextInt();
+		
+		int sum = 0;
+		
+		System.out.println(sumof(a, b));
+		
+		
 	}
 
 }
