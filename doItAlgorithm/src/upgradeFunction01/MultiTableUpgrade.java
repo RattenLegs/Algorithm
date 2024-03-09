2d3e0db87774aa71f7dabf9ff69628d394144096
Rattanlegs <Rattanlegs@gmail.com>
2024-03-09 22:46:48 +0900

    어떻게

--- doItAlgorithm/src/upgradeFunction01/MultiTableUpgrade.java
+++ doItAlgorithm/src/upgradeFunction01/MultiTableUpgrade.java
@@ -4,8 +4,6 @@ public class MultiTableUpgrade {
 
 	public static void main(String[] args) {
 	
-		//가능은 한데 의미가 없네
-		
 		for(int i=1; i<=9; i++) {
 //			System.out.println(i);
 			for(int j=1; j<=9; j++){
