cb9c74e5fabc3c0ea4b4ae6878ab2803e8e93255
Rattanlegs <Rattanlegs@gmail.com>
2024-05-16 22:04:57 +0900

    필합

---
+++ doItAlgorithm/src/chap01/upgrade/haha.java
@@ -0,0 +1,10 @@
+package chap01.upgrade;
+
+public class haha {
+
+	public static void main(String[] args) {
+	System.out.println("5 + 2 = " +3 +4);
+	System.out.println("5 + 2 = " +(3 +4));
+	}
+
+}
